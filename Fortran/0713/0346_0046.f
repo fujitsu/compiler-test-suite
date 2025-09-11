3a0a7914049b72b5aa6bc0e180ea0336ea0d6952
KAWASHIMA Takahiro <t-kawashima@fujitsu.com>
2025-09-11 17:42:12 +0900

    Remove redundant directories (Fortran/0712, Fortran/0713)
    The `Fortran/0712` directory was same as `Fortran/0207`.
    The `Fortran/0713` directory was same as `Fortran/0346`.

--- Fortran/0713/0346_0046.f
+++
@@ -1,7 +0,0 @@
-       DIMENSION NM(10000)
-       DO 10 I = 1 , 512
-            NM(I) = I
- 10    CONTINUE
-       WRITE(6,*) NM(1),NM(10),NM(100),NM(500),NM(512)
-       STOP
-       END
--- Fortran/0713/0346_0223.f
+++
@@ -1,9 +0,0 @@
-      DIMENSION  A(10),B(10),C(10),D(10),E(10),F(10),G(10)
-      DATA  A,B,C,D/10*1.0,10*2.0,10*3.0,10*4.0/
-      DO 10 I=2,10
-        A(I) = B(I)   * C(I)
-        D(I) = A(I-1) * C(I)
-   10 CONTINUE
-      WRITE(6,*) A,D
-      STOP
-      END
