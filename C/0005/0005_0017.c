6b46ffe0d4944a8ba34f35222736e92a309659b1
Maxim Solodovnik <solomax@apache.org>
2026-07-10 12:50:42 +0000

    Guards against __GLIBC_PREREQ and -std=c++11 specific code is added (#79)

--- C/0005/0005_0017.c
+++ C/0005/0005_0017.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#if defined(__GLIBC_PREREQ)
 #if __GLIBC_PREREQ(2, 17)
 #include <uchar.h>
 #endif
+#endif
 
 int main(void){
+#if defined(__GLIBC_PREREQ)
 #if __GLIBC_PREREQ(2, 17)
   char16_t  utf16;
   char32_t  utf32;
   mbstate_t mb;
   size_t    st;
+#endif
 #endif
 
   printf("ok\n");
--- C/0005/0005_0030.c
+++ C/0005/0005_0030.c
@@ -14,12 +14,14 @@ void func3(void){
 }
 
 int main(void){
+#if defined(__GLIBC_PREREQ)
 #if __GLIBC_PREREQ(2, 17)
   at_quick_exit( func3 );
   at_quick_exit( func2 );
   at_quick_exit( func1 );
 
   printf("ok\n");
+#endif
 #endif
 
   return 0;
