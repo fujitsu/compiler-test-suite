70d3621a1cb5dd4e7ccc2d8cc51a748e1699a8cd
ohno-fj <114733434+ohno-fj@users.noreply.github.com>
2026-08-19 16:27:37 +0900

    Workitem 17419  (for GCC) (#114)
    Co-authored-by: ohno <ohno.yasuyuki@jp.fujitsu.com>

--- C/0011/0011_0161.c
+++ C/0011/0011_0161.c
@@ -1,4 +1,5 @@
 #include  <stdio.h>
+#include  <string.h>
 
 int main()
 {
@@ -7,6 +8,8 @@ union {
       int  b ;
       int  c[5] ;
       } x = { 'a' } ;
+  memset(&x, 0x00, sizeof(x));
+  x.a = 'a';
   printf("********** TEST START **********\n");
 
   if (x.a=='a' && x.c[1] == 0)
