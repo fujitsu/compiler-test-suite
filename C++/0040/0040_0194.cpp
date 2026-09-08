4226fe6bf1fcf9e72806f30f236d3fd44c85c95a
ohno <ohno.yasuyuki@jp.fujitsu.com>
2026-09-08 14:52:24 +0900

    Workitem 16935  (for GCC)

--- C++/0040/0040_0194.cpp
+++ C++/0040/0040_0194.cpp
@@ -1,32 +1,12 @@
 #include "public2/004.h"
 #include <stdlib.h>
-
-namespace std
-{
-    typedef void (*terminate_handler)();
-    terminate_handler __terminate_handler = 0;
-    terminate_handler set_terminate(terminate_handler __handler) noexcept
-    {
-        terminate_handler __ret = __terminate_handler;
-        __terminate_handler = __handler;
-        return __ret;
-    }
-    terminate_handler get_terminate() noexcept
-    {
-        return __terminate_handler;
-    }
-    void terminate() noexcept
-    {
-        if (__terminate_handler)
-            __terminate_handler();
-        exit(0); 
-    }
-}
+#include <exception>
 
 void f() noexcept(true) { throw 1; }
 
 void my_terminate() noexcept
 {
+    exit(0);
 }
 
 void test()
